cc37cbc127a2803322fdb93d272c7b013d1efed0
Lucas Glonti <100687592+lack21@users.noreply.github.com>
2023-07-07 04:22:21 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// lear
+// Article Preview Component
+
+window.addEventListener("DOMContentLoaded", () => {
+  const shareBtn = document.querySelector(".share-image");
+
+  shareBtn.addEventListener("click", () => {
+    shareBtn.classList.toggle("active");
+  });
+});
